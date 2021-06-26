b938c523bf4a5e7c21c66125255635475919e967
Deividas Jablonskis <dev@d-j.site>
2021-06-26 17:51:16 +0100

    updated helpers file for missing methods

--- helpers.js
+++ helpers.js
@@ -243,3 +243,175 @@ function cC(s, p, c) {
   }
   return color;
 }
+
+function getStorageMax(capacity, storage, production) {
+  if (capacity === storage && ON_N) {
+    new Notification("Prisipilde resursai!", {
+      body: `Resources are full!`,
+      icon: "https://gpack.travian.com/20b0b1f1/mainPage/img_ltr/g/upgradeView2019/buildingIllustrations/teuton/g15.png",
+      image:
+        "https://cdnb.artstation.com/p/assets/images/images/006/367/267/large/ahmed-hmaim-final-c2.jpg?1498055051",
+    });
+  }
+
+  if (storage === 0 && production < 0 && ON_N) {
+    new Notification("Baigesi grudai!!!!", {
+      body: `Granary is empty!`,
+      icon: "https://gpack.travian.com/20b0b1f1/mainPage/img_ltr/g/upgradeView2019/buildingIllustrations/teuton/g15.png",
+      image:
+        "https://cdnb.artstation.com/p/assets/images/images/006/367/267/large/ahmed-hmaim-final-c2.jpg?1498055051",
+    });
+  }
+
+  if (production == 0) {
+    return MIN_WAIT;
+  }
+  if (production > 0) {
+    return Math.ceil(((capacity - storage) / production) * 60 * 60 * 1000);
+  }
+  return Math.ceil((storage / production) * 60 * 60 * 1000 * -1);
+}
+
+const getBuildingQueue = () => {
+  if (window.location.pathname.includes("dorf")) {
+    let buildingQ = document.querySelectorAll("div.buildingList > ul > li");
+    if (buildingQ && buildingQ.length > 0) {
+      let buildString = document
+        .querySelector("#content > script")
+        .text.includes("var bld")
+        ? document.querySelector("#content > script").text
+        : document.querySelector("#content .village1Content > script").text;
+      let buildingLevels = buildString.split("=").pop();
+      const q = JSON.parse(buildingLevels);
+      buildingQ.forEach((element, index) => {
+        if (q[index]) {
+          let seconds = Number(
+            element
+              .querySelector("div.buildDuration > span")
+              .getAttribute("value")
+          );
+          q[index].name = element
+            .querySelector("div.name")
+            .innerHTML.split("<")[0]
+            .trim();
+          q[index].finish = Date.now() + seconds * 1000;
+        }
+      });
+      return q;
+    } else return [];
+  }
+};
+
+const getCities = () => {
+  const cities = {};
+  cities.vil = [];
+
+  let loadedCities = JSON.parse(localStorage.getItem(CITIES_STORAGE));
+
+  //SETTING TRIBE
+  if (
+    loadedCities &&
+    window.location.pathname.includes("dorf1") &&
+    !Object.keys(loadedCities).includes("tribe")
+  ) {
+    cities.tribe = document.querySelector(
+      "div#resourceFieldContainer"
+    ).classList[1];
+  } else if (loadedCities && Object.keys(loadedCities).includes("tribe")) {
+    cities.tribe = loadedCities.tribe;
+  }
+
+  let villages = document.querySelectorAll(
+    "div#sidebarBoxVillagelist > div.content > ul > li"
+  );
+  villages.forEach((vil, i) => {
+    let data = vil.querySelector("a > span.coordinatesGrid");
+    let name = vil.querySelector("span.name").textContent.trim();
+    let x = Number(
+      vil
+        .querySelector("span.coordinateX")
+        .textContent.trim()
+        .slice(1)
+        .replace(/\u202c|\u202d|/g, "")
+        .replace(/\u2212/g, "-")
+    );
+    let y = Number(
+      vil
+        .querySelector("span.coordinateY")
+        .textContent.trim()
+        .slice(0, -1)
+        .replace(/\u202c|\u202d|/g, "")
+        .replace(/\u2212/g, "-")
+    );
+    let did = Number(
+      getParams("?" + vil.querySelector("a").href.split("?")[1]).newdid
+    );
+
+    const v = { name, x, y, did };
+
+    if (vil.classList.contains("active")) {
+      cities.cID = did;
+    }
+
+    if (cities.cID === did && window.location.pathname.includes("dorf")) {
+      cities.cID = did;
+      v.ress = getResources();
+      v.queue = getBuildingQueue();
+      v.timestamp = Date.now();
+
+      v.l1Max = getStorageMax(
+        v.ress.capacity.l1,
+        v.ress.storage.l1,
+        v.ress.production.l1
+      );
+      v.l2Max = getStorageMax(
+        v.ress.capacity.l2,
+        v.ress.storage.l2,
+        v.ress.production.l2
+      );
+      v.l3Max = getStorageMax(
+        v.ress.capacity.l3,
+        v.ress.storage.l3,
+        v.ress.production.l3
+      );
+      v.l4Max = getStorageMax(
+        v.ress.capacity.l4,
+        v.ress.storage.l4,
+        v.ress.production.l4
+      );
+
+      v.nextRessCheck = Math.min(v.l1Max, v.l2Max, v.l3Max, v.l4Max);
+
+      cities.vil.push(v);
+      cities.current = v;
+    } else {
+      if (loadedCities) {
+        let old = loadedCities.vil.find((x) => x.did === v.did);
+        if (old) {
+          old.name = name;
+          cities.vil.push(old);
+        } else {
+          cities.vil.push(v);
+        }
+      }
+    }
+
+    //SETING CAPITAL
+    if (window.location.pathname === "/profile") {
+      const cap = document
+        .querySelector("td.name > .mainVillage")
+        .parentNode.querySelector("a")
+        .innerText.trim();
+
+      if (name === cap) {
+        cities.cap = did;
+      }
+    } else {
+      if (loadedCities && loadedCities.cap) {
+        cities.cap = loadedCities.cap;
+      }
+    }
+  });
+  localStorage.setItem(CITIES_STORAGE, JSON.stringify(cities));
+  return cities;
+};
