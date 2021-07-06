9eb0853d4471504687efa12d61b1ffbc59a45750
Deividas Jablonskis <dev@d-j.site>
2021-07-06 20:37:27 +0100

    Notifications updated

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const VER = "0.10.9";
+const VER = "0.10.91";
 const APP_NAME = "S8nLTU BOT";
 
 const CITIES_STORAGE = "storedCities";
--- notifications.js
+++ notifications.js
@@ -1,18 +1,47 @@
-const ON_N = localStorage.getItem(BOT_NOTIFICATIONS) === ON;
+const initNotifications = () => {
+  const on = false;
+  const setting = localStorage.getItem(BOT_NOTIFICATIONS);
+  if (setting) on = setting === ON;
 
-function notifyMe(con, village) {
-  if (ON_N === ON) {
-    var notification = new Notification(
-      `${BDB.data(con.gid).name} lvl${con.lvl} completed`,
-      {
-        body: `${BDB.data(con.gid).name} was upgraded to level ${
-          con.lvl
-        } in village ${village.name}`,
-        icon: "https://emoji.gg/assets/emoji/5163-95-crythumbsup.png",
-      }
-    );
-    notification.onclick = function () {
-      if (parent) parent.focus();
-    };
+  const save = () => localStorage.setItem(BOT_NOTIFICATIONS, on ? ON : OFF);
+
+  const toggle = () => {
+    if (Notification.permission === "denied") {
+      alert(
+        "Notification permission was previously denied. Please enable notifications manualy in your browser if you want to use it."
+      );
+      on = false;
+    } else if (
+      Notification.permission !== "granted" &&
+      Notification.permission !== "denied"
+    ) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          on = !on;
+        } else {
+          alert(
+            "Notification permission was denied. Please enable notifications manualy in your browser if you want to use it."
+          );
+          on = false;
+        }
+      });
+    } else if (Notification.permission === "granted") {
+      on = !on;
+    }
+    save();
+    return on;
+  };
+
+  function send(con, village) {
+    new Notification(`${BDB.data(con.gid).name} lvl${con.lvl} completed`, {
+      body: `${BDB.data(con.gid).name} was upgraded to level ${
+        con.lvl
+      } in village ${village.name}`,
+      icon: "https://emoji.gg/assets/emoji/5163-95-crythumbsup.png",
+    });
   }
-}
+
+  return { toggle, send, on };
+};
+
+const Notifications = initNotifications();
