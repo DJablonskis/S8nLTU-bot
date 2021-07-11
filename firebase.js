31815f5219230fdc529b7ae068c9d97525f3ef80
Deividas Jablonskis <dev@d-j.site>
2021-07-11 18:56:03 +0100

    added firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAL2StXZAjB1B16Rwr-Ma13B87CesBzh14",
+  authDomain: "s8nltu.firebaseapp.com",
+  projectId: "s8nltu",
+  storageBucket: "s8nltu.appspot.com",
+  messagingSenderId: "796423177082",
+  appId: "1:796423177082:web:2eda948b4502ea450e0cc3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
